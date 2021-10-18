c71c0e4fa3a23dba408251f1e62c0fc822d63656
ogiesamosir <ogiesamosir@gmail.com>
2021-10-18 16:16:56 +0700

    Updates

---
+++ precache-manifest.96a5df74bba4c312b9c407f28c233573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46a1d0045acbe6189396",
+    "url": "/static/css/main.336e141d.chunk.css"
+  },
+  {
+    "revision": "46a1d0045acbe6189396",
+    "url": "/static/js/main.a1b13cae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "4c7307edb59b01b7250488adbab91b1c",
+    "url": "/index.html"
+  }
+];
