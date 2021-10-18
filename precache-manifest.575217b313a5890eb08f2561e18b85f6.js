8dd6fc4dc1607b84b24d200aee7c6beb45c3af59
ogiesamosir <ogiesamosir@gmail.com>
2021-10-18 16:43:36 +0700

    Updates

---
+++ precache-manifest.575217b313a5890eb08f2561e18b85f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a86d7cbb5fc83c150406",
+    "url": "/julius-samosir/static/css/main.336e141d.chunk.css"
+  },
+  {
+    "revision": "a86d7cbb5fc83c150406",
+    "url": "/julius-samosir/static/js/main.037872b7.chunk.js"
+  },
+  {
+    "revision": "d3a710ad76e573abb42c",
+    "url": "/julius-samosir/static/js/runtime~main.7d6c0650.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/julius-samosir/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "ebc95288960fcd37d40af8bee4ab10f6",
+    "url": "/julius-samosir/index.html"
+  }
+];
